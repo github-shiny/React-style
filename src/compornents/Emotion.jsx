dec77c3f40fbd47572668fc42cedee4c3aec6a1e
github-shiny <125532854+github-shiny@users.noreply.github.com>
2023-07-19 19:06:27 +0900

    Initial commit

---
+++ src/compornents/Emotion.jsx
@@ -0,0 +1,12 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "/node_modules/@emotion/serialize/dist/emotion-serialize.cjs.dev.js";
+
+export const Emotion = () => {
+  return (
+    <div>
+      <p>- Emotion -</p>
+      <button>FIGHT!</button>
+    </div>
+  );
+};
